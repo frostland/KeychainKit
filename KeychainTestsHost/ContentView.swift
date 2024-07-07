d744016295456bebba76f432343dd1bb6b678f52
Frizlab <fload@me.com>
2024-07-08 00:58:46 +0200

    Make it possible to test the project

---
+++ KeychainTestsHost/ContentView.swift
@@ -0,0 +1,24 @@
+/*
+ * ContentView.swift
+ * KeychainTestsHost
+ *
+ * Created by François Lamboley on 2024/07/08.
+ *
+ */
+
+import SwiftUI
+
+
+
+struct ContentView: View {
+	
+	var body: some View {
+		VStack { }
+	}
+	
+}
+
+
+#Preview {
+	ContentView()
+}
---
+++ KeychainTestsHost/KeychainTestsHostApp.swift
@@ -0,0 +1,22 @@
+/*
+ * KeychainTestsHostApp.swift
+ * KeychainTestsHost
+ *
+ * Created by François Lamboley on 2024/07/08.
+ *
+ */
+
+import SwiftUI
+
+
+
+@main
+struct KeychainTestsHostApp: App {
+	
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
+	
+}
