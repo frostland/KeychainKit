c01c3dde0495dbb1501d5fd60d59955b11fd5559
Frizlab <fload@me.com>
2024-07-08 01:32:24 +0200

    Add more test regarding the access groups

--- Tests/KeychainTests.swift
+++ Tests/KeychainTests.swift
@@ -7,10 +7,12 @@ final class KeychainTests: XCTestCase {
 	
 	func testBasicStorage() throws {
 		let data = Data("hello!".utf8)
+		let accessGroup = "DVL8GW97S8.fr.frostland.KeychainTestsHost"
 		let keychainID = "fr.frostland.Keychain.testBasicStorage"
 		XCTAssertNil(try Keychain.getStoredData(withIdentifier: keychainID))
 		XCTAssertNoThrow(try Keychain.setStoredData(data, withIdentifier: keychainID))
 		XCTAssertEqual(try Keychain.getStoredData(withIdentifier: keychainID), data)
+		XCTAssertEqual(try Keychain.getStoredData(withIdentifier: keychainID, accessGroup: accessGroup), data)
 		XCTAssertNoThrow(try Keychain.removeStoredData(withIdentifier: keychainID))
 		XCTAssertNil(try Keychain.getStoredData(withIdentifier: keychainID))
 	}
@@ -25,13 +27,20 @@ final class KeychainTests: XCTestCase {
 	
 	func testBasicStorageAccessGroup() throws {
 		let data = Data("hello!".utf8)
-		let accessGroup = "DVL8GW97S8.fr.frostland.KeychainTestsHost.shared"
+		let accessGroup = "DVL8GW97S8.fr.frostland.KeychainTestsHost"
+		let accessGroupShared = "DVL8GW97S8.fr.frostland.KeychainTestsHost.shared"
 		let keychainID = "fr.frostland.Keychain.testBasicStorageAccessGroup"
 		XCTAssertNil(try Keychain.getStoredData(withIdentifier: keychainID, accessGroup: accessGroup))
-		XCTAssertNoThrow(try Keychain.setStoredData(data, withIdentifier: keychainID, accessGroup: accessGroup))
-		XCTAssertEqual(try Keychain.getStoredData(withIdentifier: keychainID, accessGroup: accessGroup), data)
-		XCTAssertNoThrow(try Keychain.removeStoredData(withIdentifier: keychainID, accessGroup: accessGroup))
+		XCTAssertNil(try Keychain.getStoredData(withIdentifier: keychainID, accessGroup: accessGroupShared))
+		XCTAssertNoThrow(try Keychain.setStoredData(data, withIdentifier: keychainID, accessGroup: accessGroupShared))
+		XCTAssertEqual(try Keychain.getStoredData(withIdentifier: keychainID, accessGroup: accessGroupShared), data)
+		/* If the access group is not specified, all the access groups are searched.
+		 * I think it is not possible to change this behavious (to search only the default one):
+		 *  if this is needed, the default access group must be manually specified. */
+		XCTAssertEqual(try Keychain.getStoredData(withIdentifier: keychainID), data)
 		XCTAssertNil(try Keychain.getStoredData(withIdentifier: keychainID, accessGroup: accessGroup))
+		XCTAssertNoThrow(try Keychain.removeStoredData(withIdentifier: keychainID, accessGroup: accessGroupShared))
+		XCTAssertNil(try Keychain.getStoredData(withIdentifier: keychainID, accessGroup: accessGroupShared))
 	}
 	
 	func testClearKeychainAccessGroup() throws {
