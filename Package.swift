f4c948e6c41584174061e2c4d50e4443aff5101d
Frizlab <fload@me.com>
2024-07-08 00:21:54 +0200

    Enable strict concurrency checks

--- Package.swift
+++ Package.swift
@@ -1,12 +1,14 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.8
 import PackageDescription
 
+
+let swiftSettings: [SwiftSetting] = [.enableExperimentalFeature("StrictConcurrency")]
 
 let package = Package(
 	name: "Keychain",
 	products: [.library(name: "Keychain", targets: ["Keychain"])],
 	targets: [
-		.target(name: "Keychain", path: "Sources"),
-		.testTarget(name: "KeychainTests", dependencies: ["Keychain"], path: "Tests")
+		.target    (name: "Keychain",                                  path: "Sources", swiftSettings: swiftSettings),
+		.testTarget(name: "KeychainTests", dependencies: ["Keychain"], path: "Tests",   swiftSettings: swiftSettings),
 	]
 )
