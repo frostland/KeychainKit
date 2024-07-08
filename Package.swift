893ba8f2606384a514f5e3b9516836e774592683
Frizlab <fload@me.com>
2024-07-08 14:36:16 +0200

    Set the module name to KeychainKit instead of Keychain
    This fixes a warning due to Swift not dealing well with entities having the same names as the module name.

--- Package.swift
+++ Package.swift
@@ -5,10 +5,12 @@ import PackageDescription
 let swiftSettings: [SwiftSetting] = [.enableExperimentalFeature("StrictConcurrency")]
 
 let package = Package(
-	name: "Keychain",
-	products: [.library(name: "Keychain", targets: ["Keychain"])],
+	name: "KeychainKit",
+	/* The name of the library is KeychainKit instead of just Keychain because we have an enum called Keychain in the module and Swift does not deal with this correctly.
+	 * Once <https://github.com/swiftlang/swift/issues/56573> is fixed (if ever), we’ll probably revert the name back to just “Keychain”. */
+	products: [.library(name: "KeychainKit", targets: ["KeychainKit"])],
 	targets: [
-		.target    (name: "Keychain",                                  path: "Sources", swiftSettings: swiftSettings),
-		.testTarget(name: "KeychainTests", dependencies: ["Keychain"], path: "Tests",   swiftSettings: swiftSettings),
+		.target    (name: "KeychainKit",                                     path: "Sources", swiftSettings: swiftSettings),
+		.testTarget(name: "KeychainKitTests", dependencies: ["KeychainKit"], path: "Tests",   swiftSettings: swiftSettings),
 	]
 )
