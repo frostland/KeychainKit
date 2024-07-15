218b5bb9ef8dc9a5a58f40a41f307de0936dc329
Frizlab <fload@me.com>
2024-07-15 12:46:04 +0200

    Add all remaining attributes in GenericPassword

--- Sources/Keychain.GenericPassword.swift
+++ Sources/Keychain.GenericPassword.swift
@@ -9,6 +9,12 @@ public extension Keychain {
 	struct GenericPassword {
 		
 		public static nonisolated(unsafe) let securityClass: CFString = kSecClassGenericPassword
+		
+		/**
+		 The attributes in the generic password entry.
+		 
+		 You should rarely have to use this property directly.
+		 Instead you should use the conveniences that access/modify it. */
 		public var attributes: [CFString: Any]
 		
 	}
@@ -43,6 +49,7 @@ public extension Keychain.GenericPassword {
 
 public extension Keychain.GenericPassword {
 	
+	/** Access to `kSecAttrSynchronizable`. */
 	var synchronizable: Bool? {
 		get {
 			switch attributes[kSecAttrSynchronizable] {
@@ -64,41 +71,119 @@ public extension Keychain.GenericPassword {
 		}
 	}
 	
+	/** Access to `kSecAttrAccessGroup`. */
 	var accessGroup: String? {
 		get {typedAttribute(for: kSecAttrAccessGroup)}
 		set {attributes[kSecAttrAccessGroup] = newValue as CFString?}
 	}
 	
+	/** Access to `kSecAttrAccessControl`. */
+	var accessControl: SecAccessControl? {
+		get {typedAttribute(for: kSecAttrAccessControl)}
+		set {attributes[kSecAttrAccessControl] = newValue}
+	}
+	
+	/** Access to `kSecAttrAccount`. */
 	var account: String? {
 		get {typedAttribute(for: kSecAttrAccount)}
 		set {attributes[kSecAttrAccount] = newValue as CFString?}
 	}
 	
+	/** Access to `kSecAttrService`. */
 	var service: String? {
-		get {typedAttribute(for: kSecAttrAccount)}
+		get {typedAttribute(for: kSecAttrService)}
 		set {attributes[kSecAttrService] = newValue as CFString?}
 	}
 	
+	/** Access to `kSecAttrGeneric`. */
 	var generic: Data? {
-		get {typedAttribute(for: kSecAttrAccount)}
+		get {typedAttribute(for: kSecAttrGeneric)}
 		set {attributes[kSecAttrGeneric] = newValue as CFData?}
 	}
 	
-	/* Type should technically be an enum, but Security does not have it, so I ain’t gonna do it.
-	 * - kSecAttrAccessibleWhenPasscodeSetThisDeviceOnly
-	 * - kSecAttrAccessibleWhenUnlockedThisDeviceOnly
-	 * - kSecAttrAccessibleWhenUnlocked
-	 * - kSecAttrAccessibleAfterFirstUnlockThisDeviceOnly
-	 * - kSecAttrAccessibleAfterFirstUnlock
-	 * - (deprecated) kSecAttrAccessibleAlwaysThisDeviceOnly
-	 * - (deprecated) kSecAttrAccessibleAlways */
+	/**
+	 Access to `kSecAttrAccessible`.
+	 
+	 Type should technically be an enum, but Security does not have it, so I ain’t gonna do it.
+	 Possible values:
+	 - `kSecAttrAccessibleWhenPasscodeSetThisDeviceOnly`
+	 - `kSecAttrAccessibleWhenUnlockedThisDeviceOnly`
+	 - `kSecAttrAccessibleWhenUnlocked`
+	 - `kSecAttrAccessibleAfterFirstUnlockThisDeviceOnly`
+	 - `kSecAttrAccessibleAfterFirstUnlock`
+	 - (deprecated) `kSecAttrAccessibleAlwaysThisDeviceOnly`
+	 - (deprecated) `kSecAttrAccessibleAlways` */
 	var accessibility: CFString? {
 		get {typedAttribute(for: kSecAttrAccessible)}
 		set {attributes[kSecAttrAccessible] = newValue}
 	}
 	
-	/* No convenience access for:
-	 * - kSecAttrAccess (macOS only, softly deprecated (SecKeychain only))
-	 * - kSecAttrAccessControl */
+	/* No convenience access for kSecAttrAccess (macOS only, softly deprecated (SecKeychain only)) */
+	
+	/* Primary key:
+	 * - kSecAttrAccessGroup
+	 * - kSecAttrAccount
+	 * - kSecAttrService
+	 * - kSecAttrSynchronizable */
+	
+	/** Access to `kSecAttrCreationDate`. */
+	var creationDate: Date? {
+		typedAttribute(for: kSecAttrCreationDate)
+	}
+	
+	/** Access to `kSecAttrModificationDate`. */
+	var modificationDate: Date? {
+		typedAttribute(for: kSecAttrModificationDate)
+	}
+	
+	/** Access to `kSecAttrDescription` (user-visible, non-user-editable). */
+	var description: String? {
+		get {typedAttribute(for: kSecAttrDescription)}
+		set {attributes[kSecAttrDescription] = newValue as CFString?}
+	}
+	
+	/** Access to `kSecAttrComment` (user-visible and user-editable). */
+	var comment: String? {
+		get {typedAttribute(for: kSecAttrComment)}
+		set {attributes[kSecAttrComment] = newValue as CFString?}
+	}
+	
+	/** Access to `kSecAttrCreator` (this is a FourCC). */
+	var creator: UInt32? {
+		get {typedAttribute(for: kSecAttrCreator)}
+		set {attributes[kSecAttrCreator] = newValue as CFNumber?}
+	}
+	
+	/** Access to `kSecAttrType` (this is a FourCC). */
+	var type: UInt32? {
+		get {typedAttribute(for: kSecAttrType)}
+		set {attributes[kSecAttrType] = newValue as CFNumber?}
+	}
+	
+	/** Access to `kSecAttrLabel` (user-visible, non-user-editable). */
+	var label: String? {
+		get {typedAttribute(for: kSecAttrLabel)}
+		set {attributes[kSecAttrLabel] = newValue as CFString?}
+	}
+	
+	/**
+	 Access to `kSecAttrIsInvisible`.
+	 
+	 If the value is `nil`, the item is not invisible.
+	 We left a nullable value to be able to update entries in the keychain w/o touching the invisibility attribute. */
+	var isInvisible: Bool? {
+		get {typedAttribute(for: kSecAttrIsInvisible)}
+		set {attributes[kSecAttrIsInvisible] = newValue as CFBoolean?}
+	}
+	
+	/**
+	 Access to `kSecAttrIsNegative`.
+	 
+	 If the value is `nil`, the item is not negative.
+	 We left a nullable value to be able to update entries in the keychain w/o touching the negativity attribute. */
+	var isNegative: Bool? {
+		get {typedAttribute(for: kSecAttrIsNegative)}
+		set {attributes[kSecAttrIsNegative] = newValue as CFBoolean?}
+	}
 	
 }
